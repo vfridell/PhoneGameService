327d303870c36154f4d1cc425977f3ffe9d27b86
unknown <vfridell@IT-vfridell.bellarmine.edu>
2014-03-21 15:05:24 -0400

    initial commit

---
+++ Models/EdgeConditionals/EdgeConditional.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PhoneGameService.Models.GameStates;
+using PhoneGameService.Repositories;
+
+namespace PhoneGameService.Models.EdgeConditionals
+{
+    public abstract class EdgeConditional
+    {
+        public virtual int id { get; set; }
+        public virtual string text { get; set; }
+        public virtual GameStateNode nextNode { get; set; }
+
+        public abstract bool Transition(Game game, TelephoneGameRepository repository);
+
+        protected virtual void ChangeState(Game game, TelephoneGameRepository repository)
+        {
+            if (!game.currentNode.edgeConditionals.Contains(this))
+            {
+                throw new Exception("EdgeConditional is not in the GameStateNode");
+            }
+
+            game._currentNodeNumber = nextNode.id;
+        }
+    }
+}
